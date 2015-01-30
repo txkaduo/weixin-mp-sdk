e7b11778bd25fe58f41a7ab43923cc176784b70e
Gilbert Fine <gilbert.fine@gmail.com>
2015-01-30 18:09:30 +0800

    two new functions for menus

--- WeiXin/PublicPlatform/Menu.hs
+++ WeiXin/PublicPlatform/Menu.hs
@@ -23,3 +23,28 @@ wxppCreateMenu (AccessToken access_token) menus = do
                     >>= liftM (view responseBody) . asJSON
     when ( err /= WxppErrorX (Right WxppNoError) ) $ do
         throwM err_resp
+
+
+-- | 调用服务器接口，查询菜单
+wxppQueryMenu ::
+    ( MonadIO m, MonadLogger m, MonadThrow m) =>
+    AccessToken -> m [MenuItem]
+wxppQueryMenu (AccessToken access_token) = do
+    let url = wxppRemoteApiBaseUrl <> "/menu/get"
+        opts = defaults & param "access_token" .~ [ access_token ]
+    (liftIO $ getWith opts url)
+                >>= asWxppWsResponseNormal'
+
+
+-- | 调用服务器接口，删除菜单
+wxppDeleteMenu ::
+    ( MonadIO m, MonadLogger m, MonadThrow m) =>
+    AccessToken -> m ()
+wxppDeleteMenu (AccessToken access_token) = do
+    let url = wxppRemoteApiBaseUrl <> "/menu/delete"
+        opts = defaults & param "access_token" .~ [ access_token ]
+    err_resp@(WxppAppError err _msg) <-
+                (liftIO $ getWith opts url)
+                    >>= liftM (view responseBody) . asJSON
+    when ( err /= WxppErrorX (Right WxppNoError) ) $ do
+        throwM err_resp
