24131d77a55fb7ffbf20d7d45c6c09a163cb5a6b
Gilbert Fine <gilbert.fine@gmail.com>
2015-02-25 09:52:38 +0800

    重新设计数据库表结构，使用一个表记录收到的信息，一个表记录各种引用到的media

--- WeiXin/PublicPlatform/Yesod/Site/Data.hs
+++ WeiXin/PublicPlatform/Yesod/Site/Data.hs
@@ -53,8 +53,8 @@ newtype WxppSubDBActionRunner m = WxppSubDBActionRunner
 #if MIN_VERSION_persistent(2, 0, 0)
                     (forall backend a.
                         ( PersistStore backend
-                        , PersistEntityBackend WxppIncomingRawMsg ~ backend
-                        , PersistEntityBackend WxppIncomingHeader ~ backend
+                        , PersistEntityBackend WxppInMsgRecord ~ backend
+                        , PersistEntityBackend WxppStoredMedia ~ backend
                         ) =>
                         ReaderT backend m a -> m a
                     )
@@ -68,15 +68,23 @@ newtype WxppSubDBActionRunner m = WxppSubDBActionRunner
 #endif
         }
 
--- | 保存所有收到的信息到数据库
-data StoreMessageToDB m = StoreMessageToDB (WxppSubDBActionRunner m)
+-- | 保存所有收到的比较原始的消息（解密之后的结果）到数据库
+data StoreInMsgToDB m = StoreInMsgToDB
+                            (WxppSubDBActionRunner m)
+                                -- ^ function to run DB actions
+                            (WxppInMsgRecordId -> WxppMediaID -> m ())
+                                -- ^ function to download media file
+                                -- 推荐使用异步方式下载
 
-instance JsonConfigable (StoreMessageToDB m) where
-    type JsonConfigableUnconfigData (StoreMessageToDB m) = WxppSubDBActionRunner m
+instance JsonConfigable (StoreInMsgToDB m) where
+    type JsonConfigableUnconfigData (StoreInMsgToDB m) =
+            ( WxppSubDBActionRunner m
+            , WxppInMsgRecordId -> WxppMediaID -> m ()
+            )
 
     isNameOfInMsgHandler _ = ( == "db-store-all" )
 
-    parseInMsgHandler _ _obj = return StoreMessageToDB
+    parseInMsgHandler _ _obj = return $ uncurry StoreInMsgToDB
 
 
 instance (MonadIO m
@@ -85,18 +93,29 @@ instance (MonadIO m
     , MonadLogger m
     , MonadThrow m
 #endif
-    ) => IsWxppInMsgHandler m (StoreMessageToDB m) where
-    handleInMsg (StoreMessageToDB db_runner) _acid _get_atk bs m_ime = do
+    ) => IsWxppInMsgHandler m (StoreInMsgToDB m) where
+    handleInMsg (StoreInMsgToDB db_runner media_downloader) _acid _get_atk bs m_ime = do
         now <- liftIO getCurrentTime
-        runWxppSubDBActionRunner db_runner $ do
-            m_header_id <- forM m_ime $ \ime -> do
-                            insert $ WxppIncomingHeader
-                                        (wxppInToUserName ime)
-                                        (wxppInFromUserName ime)
-                                        (wxppInCreatedTime ime)
-                                        (wxppInMessageID ime)
-                                        now
-            insert_ $ WxppIncomingRawMsg
-                            m_header_id
+        (msg_record_id, mids) <- runWxppSubDBActionRunner db_runner $ do
+            let m_to        = fmap wxppInToUserName m_ime
+                m_from      = fmap wxppInFromUserName m_ime
+                m_ctime     = fmap wxppInCreatedTime m_ime
+                m_msg_id    = join $ fmap wxppInMessageID m_ime
+            msg_record_id <- insert $ WxppInMsgRecord
+                            m_to m_from m_ctime m_msg_id
                             (LB.toStrict bs)
+                            now
+
+            -- save any temporary media data
+            mids <- fmap (fromMaybe []) $ forM m_ime $ \ime -> do
+                        case wxppInMessage ime of
+                            WxppInMsgImage mid _   -> return [mid]
+                            WxppInMsgVoice mid _ _ -> return [mid]
+                            WxppInMsgVideo mid mid2 -> return [mid, mid2]
+                            _                       -> return []
+            return (msg_record_id, mids)
+
+        forM_ mids $ \mid -> do
+            media_downloader msg_record_id mid
+
         return $ Right Nothing
