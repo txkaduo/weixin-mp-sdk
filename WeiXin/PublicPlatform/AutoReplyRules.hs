2006cf8b43bd58eb08c9acd755135946b1e90219
Gilbert Fine <gilbert.fine@gmail.com>
2015-05-05 17:53:02 +0800

    实现获取当前自动回复规则

---
+++ WeiXin/PublicPlatform/AutoReplyRules.hs
@@ -0,0 +1,24 @@
+module WeiXin.PublicPlatform.AutoReplyRules
+    ( module WeiXin.PublicPlatform.AutoReplyRules
+    , module WeiXin.PublicPlatform.Types
+    ) where
+
+import ClassyPrelude
+import Network.Wreq
+import Control.Lens
+import Control.Monad.Logger
+import Data.Aeson
+
+import WeiXin.PublicPlatform.Types
+import WeiXin.PublicPlatform.WS
+
+
+-- | 获取自动回复规则
+wxppQueryOriginAutoReplyRules :: ( MonadIO m, MonadLogger m, MonadThrow m) =>
+    AccessToken
+    -> m Object
+wxppQueryOriginAutoReplyRules (AccessToken atk) = do
+    let url = wxppRemoteApiBaseUrl <> "/get_current_autoreply_info"
+        opts = defaults & param "access_token" .~ [ atk ]
+    (liftIO $ getWith opts url)
+            >>= asWxppWsResponseNormal'
