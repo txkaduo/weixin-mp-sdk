e82a61859a60db5a5d57e2cf52b9ab5f09f46e40
Gilbert Fine <gilbert.fine@gmail.com>
2015-05-25 19:47:07 +0800

    implement a cache backend using sql database

--- WeiXin/PublicPlatform/Yesod/Site/Function.hs
+++ WeiXin/PublicPlatform/Yesod/Site/Function.hs
@@ -246,3 +246,117 @@ downloadSaveMediaToDB atk msg_id media_id = do
                                             <> "' already in DB, record id: "
                                             <> toPathPiece old_id
                         Right _ -> return ()
+
+
+instance WxppCacheBackend (WxppSubDBActionRunner IO) where
+    wxppCacheGetAccessToken (WxppSubDBActionRunner run_db) app_id = do
+        run_db $ do
+            fmap (fmap $ flip AccessToken app_id . wxppCachedAccessTokenData . entityVal) $
+                selectFirst
+                    [ WxppCachedAccessTokenApp ==. app_id ]
+                    [ Desc WxppCachedAccessTokenCreatedTime ]
+
+    wxppCacheAddAccessToken (WxppSubDBActionRunner run_db) atk expiry = do
+        now <- liftIO getCurrentTime
+        run_db $ do
+            insert_ $ WxppCachedAccessToken
+                            (accessTokenApp atk)
+                            (accessTokenData atk)
+                            expiry
+                            now
+
+    wxppCachePurgeAccessToken (WxppSubDBActionRunner run_db) expiry = do
+        run_db $ do
+            deleteWhere [ WxppCachedAccessTokenExpiryTime <=. expiry ]
+
+    wxppCacheLookupUserInfo (WxppSubDBActionRunner run_db) app_id open_id = do
+        run_db $ do
+            fmap
+                (fmap $
+                    (fromWxppCachedUserInfoExt &&& wxppCachedUserInfoExtCreatedTime) .
+                    entityVal
+                ) $
+                getBy $ UniqueWxppCachedUserInfoExt open_id app_id
+
+    wxppCacheSaveUserInfo (WxppSubDBActionRunner run_db) app_id qres = do
+        now <- liftIO getCurrentTime
+        run_db $ do
+            case toWxppCachedUserInfoExt app_id now qres of
+                Nothing -> do
+                    -- 用户不再关注
+                    -- 暂时不删除缓存
+                    return ()
+
+                Just rec -> do
+                    insertBy rec
+                        >>= either (flip replace rec . entityKey) (const $ return ())
+
+    wxppCacheLookupUploadedMediaIDByHash (WxppSubDBActionRunner run_db) app_id h = do
+        run_db $ do
+            m_rec <- getBy $ UniqueWxppCachedUploadedMediaHash h app_id
+            case entityVal <$> m_rec of
+                Nothing -> return Nothing
+                Just (WxppCachedUploadedMedia _app_id _md5 mtype mid ctime) -> do
+                    return $ Just $ UploadResult mtype mid ctime
+
+    wxppCacheSaveUploadedMediaID
+        (WxppSubDBActionRunner run_db) app_id h (UploadResult mtype mid ctime) = do
+        run_db $ do
+            let rec = WxppCachedUploadedMedia app_id h mtype mid ctime
+            insertBy rec >>= either (flip replace rec . entityKey) (const $ return ())
+
+
+fromWxppCachedUserInfoExt :: WxppCachedUserInfoExt -> EndUserQueryResult
+fromWxppCachedUserInfoExt
+    (WxppCachedUserInfoExt
+        _app_id
+        open_id
+        m_union_id
+        nickname
+        m_gender
+        locale
+        city
+        province
+        country
+        head_img
+        subs_time
+        _create_time)
+    = EndUserQueryResult
+        open_id
+        nickname
+        m_gender
+        locale
+        city
+        province
+        country
+        head_img
+        subs_time
+        m_union_id
+
+toWxppCachedUserInfoExt :: WxppAppID -> UTCTime -> EndUserQueryResult -> Maybe WxppCachedUserInfoExt
+toWxppCachedUserInfoExt _ _ (EndUserQueryResultNotSubscribed {}) = Nothing
+toWxppCachedUserInfoExt app_id created_time
+    (EndUserQueryResult
+        open_id
+        nickname
+        m_gender
+        locale
+        city
+        province
+        country
+        head_img
+        subs_time
+        m_union_id)
+    = Just $ WxppCachedUserInfoExt
+        app_id
+        open_id
+        m_union_id
+        nickname
+        m_gender
+        locale
+        city
+        province
+        country
+        head_img
+        subs_time
+        created_time
