3b4696e6f0ad9817f309be8003ccb44039eda28a
Gilbert Fine <gilbert.fine@gmail.com>
2015-08-22 16:54:13 +0800

    add a top level module for convenience

---
+++ WeiXin/PublicPlatform.hs
@@ -0,0 +1,51 @@
+module WeiXin.PublicPlatform
+    ( module WeiXin.PublicPlatform.Error
+    , module WeiXin.PublicPlatform.Types
+    , module WeiXin.PublicPlatform.Class
+    , module WeiXin.PublicPlatform.WS
+    , module WeiXin.PublicPlatform.Acid
+    , module WeiXin.PublicPlatform.Security
+    , module WeiXin.PublicPlatform.Media
+    , module WeiXin.PublicPlatform.Material
+    , module WeiXin.PublicPlatform.AutoReplyRules
+    , module WeiXin.PublicPlatform.Message
+    , module WeiXin.PublicPlatform.InMsgHandler
+    , module WeiXin.PublicPlatform.Menu
+    , module WeiXin.PublicPlatform.CS
+    , module WeiXin.PublicPlatform.QRCode
+    , module WeiXin.PublicPlatform.EndUser
+    , module WeiXin.PublicPlatform.Propagate
+    , module WeiXin.PublicPlatform.Yesod.Site
+    , module WeiXin.PublicPlatform.Yesod.Site.Function
+    , module WeiXin.PublicPlatform.Yesod.Site.Data
+    , module WeiXin.PublicPlatform.BgWork
+    , module WeiXin.PublicPlatform.Utils
+    , module WeiXin.PublicPlatform.Misc
+    -- , module WeiXin.PublicPlatform.Conversation
+    , module WeiXin.PublicPlatform.Center
+    ) where
+
+import WeiXin.PublicPlatform.Error
+import WeiXin.PublicPlatform.Types
+import WeiXin.PublicPlatform.Class
+import WeiXin.PublicPlatform.WS
+import WeiXin.PublicPlatform.Acid
+import WeiXin.PublicPlatform.Security
+import WeiXin.PublicPlatform.Media
+import WeiXin.PublicPlatform.Material
+import WeiXin.PublicPlatform.AutoReplyRules
+import WeiXin.PublicPlatform.Message
+import WeiXin.PublicPlatform.InMsgHandler
+import WeiXin.PublicPlatform.Menu
+import WeiXin.PublicPlatform.CS
+import WeiXin.PublicPlatform.QRCode
+import WeiXin.PublicPlatform.EndUser
+import WeiXin.PublicPlatform.Propagate
+import WeiXin.PublicPlatform.Yesod.Site
+import WeiXin.PublicPlatform.Yesod.Site.Function
+import WeiXin.PublicPlatform.Yesod.Site.Data
+import WeiXin.PublicPlatform.BgWork
+import WeiXin.PublicPlatform.Utils
+import WeiXin.PublicPlatform.Misc
+-- import WeiXin.PublicPlatform.Conversation hiding (Text.Parsec.Text, Text.Parsec.Error, Text.Parsec.Prim)
+import WeiXin.PublicPlatform.Center
