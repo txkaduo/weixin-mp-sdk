7f6114892c5e6373be7d2a2764d842f3c9f76b6e
Gilbert Fine <gilbert.fine@gmail.com>
2016-09-24 00:27:40 +0800

    reexport BankCode

--- WeiXin/PublicPlatform/Pay.hs
+++ WeiXin/PublicPlatform/Pay.hs
@@ -1,8 +1,10 @@
 module WeiXin.PublicPlatform.Pay
-  ( module WeiXin.PublicPlatform.Pay.Types
+  ( module WeiXin.PublicPlatform.Pay.BankCode
+  , module WeiXin.PublicPlatform.Pay.Types
   , module WeiXin.PublicPlatform.Pay.Function
   )
   where
 
+import WeiXin.PublicPlatform.Pay.BankCode
 import WeiXin.PublicPlatform.Pay.Types
 import WeiXin.PublicPlatform.Pay.Function
