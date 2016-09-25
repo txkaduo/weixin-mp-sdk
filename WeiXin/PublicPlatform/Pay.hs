4cacf1a7167317ff2a0537348650b6711315a2c1
Gilbert Fine <gilbert.fine@gmail.com>
2016-09-25 22:17:09 +0800

    new function: yesodHandleWxUserPayNotify

--- WeiXin/PublicPlatform/Pay.hs
+++ WeiXin/PublicPlatform/Pay.hs
@@ -2,9 +2,11 @@ module WeiXin.PublicPlatform.Pay
   ( module WeiXin.PublicPlatform.Pay.BankCode
   , module WeiXin.PublicPlatform.Pay.Types
   , module WeiXin.PublicPlatform.Pay.Function
+  , module WeiXin.PublicPlatform.Pay.Yesod
   )
   where
 
 import WeiXin.PublicPlatform.Pay.BankCode
 import WeiXin.PublicPlatform.Pay.Types
 import WeiXin.PublicPlatform.Pay.Function
+import WeiXin.PublicPlatform.Pay.Yesod
---
+++ WeiXin/PublicPlatform/Pay/Yesod.hs
@@ -0,0 +1,53 @@
+module WeiXin.PublicPlatform.Pay.Yesod where
+
+import ClassyPrelude.Yesod
+
+import           Control.Monad.Except
+import           Data.Conduit.Binary  (sinkLbs)
+import           Text.XML             (renderText)
+
+import WeiXin.PublicPlatform.Types
+import WeiXin.PublicPlatform.Pay.Types
+import WeiXin.PublicPlatform.Pay.Function
+
+
+-- | 用于实现主动通知接口
+yesodHandleWxUserPayNotify :: (MonadHandler m, MonadLogger m, MonadBaseControl IO m)
+                           => WxPayAppKey
+                           -> ( WxppAppID
+                                 -> WxPayMchID
+                                 -> WxUserPayStatInfo
+                                 -> m ()
+                              )
+                           -> m RepXml
+yesodHandleWxUserPayNotify app_key handle_notify = do
+  lbs <- rawRequestBody $$ sinkLbs
+  let parse_params = runExceptT $ do
+        input_params <- ExceptT $ wxPayParseInputXmlLbs app_key lbs
+        app_id <- fmap WxppAppID $ reqXmlTextField input_params "appid"
+        mch_id <- fmap WxPayMchID $ reqXmlTextField input_params "mch_id"
+        fmap ((app_id, mch_id),) $ wxUserPayParseStateParams input_params
+
+  err_or <- try parse_params
+  out_params <- case err_or of
+    Left (WxPayDiagError err) -> do
+      $logErrorS wxppLogSource $ "Pay notification: failed to parse: " <> err
+      return $ [ ("return_code", "FAIL")
+               , ("return_msg", err)
+               ]
+
+    Right err_or_pay_stat -> do
+      case err_or_pay_stat of
+        Left err -> do
+          $logErrorS wxppLogSource $ "Pay notification: got result error: " <> tshow err
+          return $ [ ("return_code", "FAIL")
+                   , ("return_msg", "caller error")
+                   ]
+
+        Right ((app_id, mch_id), pay_stat) -> do
+          handle_notify app_id mch_id pay_stat
+          return $ [ ("return_code", "SUCCESS")
+                   , ("return_msg", "OK")
+                   ]
+
+  return $ repXml $ renderText def $ wxPayOutgoingXmlDocFromParams $ mapFromList out_params
